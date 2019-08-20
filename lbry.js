940ff68bd12da20dd408bfb384a94c311a2108fb
Melroy van den Berg <melroy@melroy.org>
2019-08-20 23:21:35 +0200

    more portable code

--- lbry.js
+++ lbry.js
@@ -20,6 +20,7 @@ class LBRY {
     // Public ChainQuery API
     this.chainquery_api = 'https://chainquery.lbry.com/api/sql'
     // CoinMarketCap
+    this.coinmarket_id = 1298 // 1298 = LBC
     this.coinmarket = axios.create({
       baseURL: 'https://pro-api.coinmarketcap.com/v1',
       timeout: 10000,
@@ -37,13 +38,13 @@ class LBRY {
   getLatestPrices () {
     return this.coinmarket.get('/cryptocurrency/quotes/latest', {
       params: {
-        id: 1298,
+        id: this.coinmarket_id,
         aux: 'num_market_pairs,cmc_rank,date_added,tags,platform,max_supply,circulating_supply,total_supply,market_cap_by_total_supply,' +
         'volume_24h_reported,volume_7d,volume_7d_reported,volume_30d,volume_30d_reported'
       }
     })
       .then(response => {
-        return Promise.resolve(response.data.data['1298'])
+        return Promise.resolve(response.data.data[this.coinmarket_id])
       })
   }
 
