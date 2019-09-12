9f26150fb63e22775ae569fc039aaac006d79fad
Melroy van den Berg <melroy@melroy.org>
2019-09-12 03:15:58 +0200

    screen

--- start_bot_prod.sh
+++ start_bot_prod.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-export $(cat tokens.env | xargs) && screen -dmS bot NODE_ENV=production node app.js
+export $(cat tokens.env | xargs) && screen -dmS bot bash -c 'NODE_ENV=production node app.js'
