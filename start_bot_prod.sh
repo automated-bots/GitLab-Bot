7e1a945d5da5d413ed6e4a9aa51a82432ce0479f
Melroy van den Berg <melroy@melroy.org>
2019-09-16 21:45:17 +0200

    typo

--- start_bot_prod.sh
+++ start_bot_prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 cd $DIR
 screen -dmS bot bash -c 'export $(cat tokens.env | xargs) && NODE_ENV=production node src/index.js'
